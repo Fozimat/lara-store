31163428c25b96f1b11ce4da734d0dc477134075
Fozimat <fozimata@gmail.com>
2021-06-21 15:58:52 +0700

    first commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="text-center">
+                    <p class="pt-4 pb-2">
+                        2020 Copyright Store. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
