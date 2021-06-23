d11b0c418f92bd36d2ac31b6a0f0ab2ae8335df5
Fozimat <fozimata@gmail.com>
2021-06-23 20:21:37 +0700

    CRUD Product Gallery done

--- app/Models/ProductGallery.php
+++ app/Models/ProductGallery.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductGallery extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'photos', 'products_id'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
 }
