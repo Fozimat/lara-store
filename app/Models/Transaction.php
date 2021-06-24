67e53765063d8625283bdfa04ee96e1da6589d41
Fozimat <fozimata@gmail.com>
2021-06-24 17:12:23 +0700

    Checkout page -1

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'users_id',
+        'inscurance_price',
+        'shipping_price',
+        'total_price',
+        'code'
+    ];
 }
